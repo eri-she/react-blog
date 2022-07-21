030ba4250a7181305675f27bd936274cf5a47adb
eri-she <96880798+eri-she@users.noreply.github.com>
2022-07-21 13:52:14 +0200

    fetch data

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,15 +1,11 @@
 export default function BlogList(props) {
   return (
     <div>
-      <h1>{props.title}</h1>
       {props.blogs.map(function (blogs, index) {
         return (
           <div key={index}>
-            <h2>{blogs.title}</h2>
+            <p>{blogs.title}</p>
             <p>written by {blogs.author}</p>
-            <button onClick={() => props.handleDelete(blogs.id)}>
-              Delete blog
-            </button>
           </div>
         );
       })}
--- src/Home.js
+++ src/Home.js
@@ -1,34 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 export default function Home() {
-  const [blogs, setBlogs] = useState([
-    {
-      title: "My website",
-      body: "a website is the...",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "My laptop",
-      body: "a laptop is the...",
-      author: "rosa",
-      id: 2,
-    },
-    {
-      title: "My coffee",
-      body: "a coffee is the...",
-      author: "julio",
-      id: 3,
-    },
-  ]);
-  function handleDelete(id) {
-    const newBlogs = blogs.filter((blogs) => blogs.id !== id);
-    setBlogs(newBlogs);
-  }
-  return (
-    <div>
-      <BlogList blogs={blogs} title="All blogs!" handleDelete={handleDelete} />
-    </div>
-  );
+  const [blogs, setBlogs] = useState([null]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setBlogs(data);
+      });
+  }, []);
+
+  return <div>{blogs && <BlogList blogs={blogs} />}</div>;
 }
