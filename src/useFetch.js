cf928f5b3fc8e4c9f8bb4e49ee0ad158ad46aa35
eri-she <96880798+eri-she@users.noreply.github.com>
2022-07-22 22:18:46 +0200

    created useFetch

---
+++ src/useFetch.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(url)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error(`We couldn´t fetch data`);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
+      });
+  }, [url]);
+  return { data, isPending, error };
+}
