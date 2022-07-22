b8192e2916bf2cb4d389cc64fa8ed89aef8ba9f5
eri-she <96880798+eri-she@users.noreply.github.com>
2022-07-22 20:15:11 +0200

    added error message

--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,31 @@ import BlogList from "./BlogList";
 export default function Home() {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error(`We couldn´t fetch data`);
+        }
         return res.json();
       })
       .then((data) => {
-        console.log(data);
         setBlogs(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setIsPending(false);
+        setError(err.message);
       });
   }, []);
 
   return (
     <div>
-      {isPending && <div> Loding...</div>}
+      {error && <div>{error}</div>}
+      {isPending && <div> Loading...</div>}
       {blogs && <BlogList blogs={blogs} />}
     </div>
   );
