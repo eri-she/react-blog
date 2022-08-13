4ab2c2bf3cb9e0e30b0aeab49a29c1a8b4f30f38
eri-she <96880798+eri-she@users.noreply.github.com>
2022-08-13 21:32:16 +0200

    added form and css

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,19 @@
 export default function Create() {
   return (
     <div>
-      <h1>New Blogs!</h1>
+      <h1 className="CreateTitle">New Blogs!</h1>
+      <form>
+        <label className="FormTitle">Blog Title</label>
+        <input type="text" required />
+        <label>Blog Body</label>
+        <textarea required></textarea>
+        <label>Author</label>
+        <select>
+          <option value="Mario">Mario </option>
+          <option value="Rosa">Rosa </option>
+        </select>
+        <button> Create New Blog</button>
+      </form>
     </div>
   );
 }
