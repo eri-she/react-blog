1aedfd9171f37dd5b10f00673b267b8de424a253
eri-she <96880798+eri-she@users.noreply.github.com>
2022-08-13 21:58:07 +0200

    added onChange

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,31 @@
+import { useState } from "react";
+
 export default function Create() {
+  const [title, setTitle] = useState(``);
+  const [body, setBody] = useState(``);
+  const [author, setAuthor] = useState(``);
   return (
     <div>
       <h1 className="CreateTitle">New Blogs!</h1>
       <form>
         <label className="FormTitle">Blog Title</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
         <label>Blog Body</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(event) => setBody(event.target.value)}
+        ></textarea>
         <label>Author</label>
-        <select>
+        <select
+          value={author}
+          onChange={(event) => setAuthor(event.target.value)}
+        >
           <option value="Mario">Mario </option>
           <option value="Rosa">Rosa </option>
         </select>
